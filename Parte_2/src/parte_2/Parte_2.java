3ea0f9af9ab53b0131220f44240918791f8fc2bd
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2023-09-24 20:29:02 -0500

    [P2] (feat) -> Inicio de separacion de metodos para cada ADDR

--- Parte_2/src/parte_2/Parte_2.java
+++ Parte_2/src/parte_2/Parte_2.java
@@ -14,12 +14,10 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Parte_2 {
-
-        
-    static Linea PrimerLinCod=null,NewLinCod=null,FinLinCod=null; //Va aguardar las variables
-    static int aux;
-    static boolean bandera = false;//Variable que indica si la linea es un comentario
+    static Linea PrimerLinCod=null,NewLinCod=null,FinLinCod=null; //Para guardar en una lista las lineas del codigo
+    static boolean Comentario = false;//Variable que indica si la linea es un comentario
     
+//**************************************************************** PARTE 1 *******************************************************
     //METODO PARA EVALUAR ETIQUETA
     static boolean validarEtiq(String etiqueta){
         boolean banEtiq=false;
@@ -65,69 +63,12 @@ public class Parte_2 {
         return banCodop;
     }//FINAL DE METODO PARA EVALUAR CODOP
     
-    //METODO DE APOYO PARA PASAR DE OCTAL A DECIMAL...
-    public static int octalADecimal(int octal) {
-    int decimal = 0;
-    int potencia = 0;
-    // Ciclo infinito que se rompe cuando octal es 0
-    while (true) {
-        if (octal == 0) {//HASTA QUE OCTAL = 0 SE PARA EL BUCLE
-            break;
-        } else {
-            int temp = octal % 10;
-            decimal += temp * Math.pow(8, potencia);
-            octal = octal / 10;
-            potencia++;
-            //PROCESO DE CONVERSION...
-        }
-    }
-    return decimal; //regresa el valor para trabajarlo
-}//FIN METODO PARA PASAR OCTAL A DECIMAL...
-   
-    //funcion para validar tipo hexadecimal
-    static boolean ValidarHexadecimal(String Hexa){
-        boolean Valido=false;  //variable para aceptar o denegar
-        if(Hexa.matches("[0-9A-F]+")){ // funcion que valida que tenga caracteres permitidos
-            if(Hexa.length()<=4){ //si tiene el num mayor permitido (FFFF), o menos. Entonces acepta...
-                Valido=true;
-            }
-        }
-        return Valido;
-    }
-    
-    //Metodo para evaluar Octal
-    static boolean validarOctal(String Oct){
-      boolean banOctal = false;
-        int Octal;
-        if(Oct.matches("[1-7]+")){ // valida que tenga numeros solo del 0 al 7
-            Octal = Integer.parseInt(Oct); //convierte de String s entero
-            if(octalADecimal(Octal)<=65535){ //revisa que sea menor o igual a 16 bits que es lo permitido
-                banOctal=true;
-            }
-        }
-      //proceso...
-      return banOctal;
-    }//Fin evaluar octal
-    
-    //Metodo para evaluar Binario
-    static boolean ValidarBinario(String nBin){
-        boolean Valido=false;  // variable que sirve para validar o denegar 
-        if(nBin.matches("[0-1]+")){ //revisa que solo tenga valores permitidos (0 y 1).
-            if(nBin.length()<=16){ //revisa que cumpla con la cantidad de bits permitidos.
-                Valido=true; //valida verdadero
-            }
-        }
-        return Valido;//regresa el valor
-        
-    }//Fin metodo para evaluar binario...
-    
-    
     //METODO IDENTIFICAR Y EVALUAR COMENTARIO
     static boolean Comentario(String x){
         boolean Coment=false;
         char c = x.charAt(0);//Para evaluar caracter por caracter
         if(c==';'){
-            bandera=true;
+            Comentario=true;
         }//Fin debe empezar con punto y coma
         if (x.length() > 81){
             Coment = true;
@@ -148,53 +89,51 @@ public class Parte_2 {
             while(cursorActual!=auxArchivo.length()){//mientras el lector no llegue al final del archivo
                 lecturaLinea = auxArchivo.readLine();//leo la linea
                 cursorActual = auxArchivo.getFilePointer();
-                bandera=false;
-                if(!(lecturaLinea.equals(""))){
+                Comentario=false;
+                if(!(lecturaLinea.equals(""))){//El if hace que se ignore cuando hay un linea de codigo vacia
                     Comentario(lecturaLinea);
                 }
-                if(!(bandera)){
+                if(!(Comentario)){ //Si no es un comentario, debe ser una linea de codigo
                     String[] campos = lecturaLinea.split("\\s+");//Separa por bloques segun cada espacios o tabulación
                     NewLinCod = new Linea(" "," "," ",null," "); //Inicializo valores
-
-                    aux=0;
-                    if(campos[0].equals("")){
-                        aux++;
-                    }//Fin si empieza con espacio
-                    if(campos.length<=aux+4){//If para validacion de solo 3 bloques no mas
-                        if(aux<campos.length){
-                            if(validarEtiq(campos[aux])&&aux<campos.length-1 && validarCodop(campos[aux+1]) && !(campos[0].equals(""))){
-                                NewLinCod.setEtiqueta(campos[aux]);
-                                aux++;//Para movernos al siguiente bloque
-                            }//Busco una etiqueta
-                            if(validarCodop(campos[aux])){
-                                NewLinCod.setCodop(campos[aux]);
-                                aux++;//Para movernos al siguiente bloque
-                                if(aux==campos.length-1){//Si hay siguiente bloque debe ser el operando
-                                   NewLinCod.setOperando(campos[aux]); 
-                                }
-                                else if(aux==campos.length-2){
-                                    NewLinCod.setOperando(campos[aux].concat(campos[aux+1]));
-                                }
-                                else if(NewLinCod.getCodop().equals("END")&&aux==campos.length){
-                                    cursorActual=auxArchivo.length();
-                                }
-                                if(PrimerLinCod==null){
-                                    PrimerLinCod=NewLinCod;
-                                }
-                                else{
-                                    FinLinCod.setSiguiente(NewLinCod);
-                                }
-                                FinLinCod=NewLinCod;
-                            }//Fin CODOP correcto
+                    if(campos.length<=4 && campos.length>1){//If para validacion de solo 4 bloques no mas
+                        if(!(campos[0].equals(""))){
+                            if(validarEtiq(campos[0])){
+                                NewLinCod.setEtiqueta(campos[0]);
+                            }
                             else{
-                                System.out.println("ERROR");
-                            }//Fin error con codigo de operacion
-                        }//Fin if para recorrer bloques del codigo
-                    }//Fin no mas de 3 codigos
+                                NewLinCod.setEtiqueta("Formato Etiqueta");
+                            }
+                        }//Busco una etiqueta
+                        if(validarCodop(campos[1])){
+                            NewLinCod.setCodop(campos[1]);
+                            if(campos.length==3){//Si hay siguiente bloque debe ser el operando
+                                NewLinCod.setOperando(campos[2]); 
+                            }
+                            else if(campos.length==4 && campos[2].contains(",")){
+                                NewLinCod.setOperando(campos[2].concat(campos[3]));
+                            }
+                            else if(NewLinCod.getCodop().equals("END")&&campos.length==2){
+                                cursorActual=auxArchivo.length();
+                            }
+                            if(PrimerLinCod==null){
+                                PrimerLinCod=NewLinCod;
+                            }
+                            else{
+                                FinLinCod.setSiguiente(NewLinCod);
+                            }
+                            FinLinCod=NewLinCod;
+                        }//Fin CODOP correcto
+                        else{
+                            System.out.println("ERROR "+campos[1]+"No es un CODOP");
+                        }//Fin error con codigo de operacion
+                    }//Fin no mas de 4 bloques
                     else{
-                        System.out.println("ERROR");
+                        if(campos.length>4){
+                            System.out.println("ERROR mas de 4 bloques en la linea");
+                        }
                     }
-                }//Fin if: no es un comentario
+                }//Fin if: no es un comentario, es una linea de codigo
                 else{
                     if (Comentario (lecturaLinea)){
                         System.out.println("COMENTARIO CON EXCESO DE CARACTERES");
@@ -212,106 +151,106 @@ public class Parte_2 {
         }
     }
     
-    //METODO PARA IDENTIFICAR ADDR SEGUN SU OPERANDO
-    static void ADDR (Linea Evaluar){
-        //HACE FALTA BUSCAR QUE EL CODOP EXISTA
-        boolean fin =false;
-        do{
-            if(Evaluar.getOperando().equals(" ")){
-                if (Evaluar.getCodop().equals("END")){
-                    Evaluar.setADDR("DIRECT");
+//******************************************************************** PARTE 2 *************************************************
+    
+    //METODO DE APOYO PARA PASAR DE OCTAL A DECIMAL...
+    public static int octalADecimal(int octal) {
+    int decimal = 0;
+    int potencia = 0;
+    // Ciclo infinito que se rompe cuando octal es 0
+    while (true) {
+        if (octal == 0) {//HASTA QUE OCTAL = 0 SE PARA EL BUCLE
+            break;
+        } else {
+            int temp = octal % 10;
+            decimal += temp * Math.pow(8, potencia);
+            octal = octal / 10;
+            potencia++;
+            //PROCESO DE CONVERSION...
+        }
+    }
+    return decimal; //regresa el valor para trabajarlo
+    }//FIN METODO PARA PASAR OCTAL A DECIMAL...
+    
+    
+    
+     //METODO PARA VALIDAR UN HEXADECIMAL
+    static boolean ValidarHexadecimal(String Hexa){
+        boolean Valido=false;  //variable para aceptar o denegar
+        if(Hexa.matches("[0-9A-F]+")){ // funcion que valida que tenga caracteres permitidos
+            if(Hexa.length()<=4){ //si tiene el num mayor permitido (FFFF), o menos. Entonces acepta...
+                Valido=true;
+            }
+        }
+        return Valido;
+    }//Fin evaluar hexadecimal
+    
+    //METODO PARA EVALUAR UN OCTAL
+    static boolean ValidarOctal(String Oct){
+      boolean banOctal = false;
+        int Octal;
+        if(Oct.matches("[1-7]+")){ // valida que tenga numeros solo del 0 al 7
+            Octal = Integer.parseInt(Oct); //convierte de String s entero
+            if(octalADecimal(Octal)<=65535){ //revisa que sea menor o igual a 16 bits que es lo permitido
+                banOctal=true;
+            }
+        }
+      //proceso...
+      return banOctal;
+    }//Fin evaluar octal
+    
+    //METODO PARA EVALUAR UN BINARIO
+    static boolean ValidarBinario(String nBin){
+        boolean Valido=false;  // variable que sirve para validar o denegar 
+        if(nBin.matches("[0-1]+")){ //revisa que solo tenga valores permitidos (0 y 1).
+            if(nBin.length()<=16){ //revisa que cumpla con la cantidad de bits permitidos.
+                Valido=true; //valida verdadero
+            }
+        }
+        return Valido;//regresa el valor
+        
+    }//Fin metodo para evaluar binario..
+    
+    //METODO PARA PASAR DE CUALQUIER BASE A DECIMAL
+    static int ConvertirADecimal(String Operando){
+        int Decimal=0;
+        switch(Operando.charAt(0)){
+            case '$':
+                if(ValidarHexadecimal(Operando.substring(1))){
+                    Decimal = Integer.parseInt(Operando, 16);
                 }
                 else{
-                    Evaluar.setADDR("INH");
+                    Decimal = -1;
                 }
-            }
-            else if(Evaluar.getCodop().equals("ORG") && Evaluar.getOperando().startsWith("$")){
-                Evaluar.setADDR("DIRECT");
-            }
-            else if(Evaluar.getCodop().equals("SEX")){
-                Evaluar.setADDR("INH");
-            }
-            else if(Evaluar.getOperando().contains(",") && !(Evaluar.getOperando().contains("[") || Evaluar.getOperando().contains("]"))){
-                String partes[]=Evaluar.getOperando().split(",");
-                if(partes[0].matches(".*\\d.*")){
-                    int valor=Integer.parseInt(partes[0]);
-                    if(partes[1].equals("X") || partes[1].equals("Y") ||
-                                partes[1].equals("SP") || partes[1].equals("PC")){
-                        if(valor>-17 && valor<15){
-                            Evaluar.setADDR("IDX(5b)");
-                        }
-                        else if((valor>-257 && valor<-16)||(valor>15 && valor<256)){
-                            Evaluar.setADDR("IDX1");
-                        }
-                        else if(valor>255 && valor<65536){
-                            Evaluar.setADDR("IDX2");
-                        }
-                    }
-                    if((partes[1].startsWith("+") || partes[1].startsWith("-") || partes[1].endsWith("+") ||
-                            partes[1].endsWith("-"))&& (partes[1].contains("X") ||
-                            partes[1].contains("Y") || partes[1].contains("SP") || partes[1].contains("PC")) ){
-                        if(valor>0 && valor<9){
-                            Evaluar.setADDR("IDX(pre-inc)");
-                        }
-                    }
+            break;
+            case '@':
+                if(ValidarOctal(Operando.substring(1))){
+                    Decimal = octalADecimal(Integer.parseInt(Operando.substring(1)));
                 }
-                else if(partes[0].equals("A")||partes[0].equals("B")||partes[0].equals("D")){
-                    if(partes[1].equals("X") || partes[1].equals("Y") ||
-                                partes[1].equals("SP") || partes[1].equals("PC")){
-                        Evaluar.setADDR("IDX(acc)");
-                    }
+                else{
+                    Decimal = -1;
                 }
-            }
-            else{
-                switch(Evaluar.getOperando().charAt(0)){
-                    case '#':
-                        Evaluar.setADDR("IMM");
-                    break;
-                    case '$':
-                        
-                    break;
-                    case '@':
-                   
-                    break;
-                    case '%':
-                    break;
-                    case '[':
-                        String partes[]=Evaluar.getOperando().split(",");
-                        if(partes[0].equals("[D")){
-                            
-                            if(partes[1].equals("X]") || partes[1].equals("Y]") ||
-                                partes[1].equals("SP]") || partes[1].equals("PC]")){
-                                   Evaluar.setADDR("[D,IDX]");
-                            }
-                        }
-                        else{
-                            String valor = partes[0].substring(1);
-                            if(valor.matches(".*\\d.*")){
-                                int numero = Integer.parseInt(valor);
-                                
-                                if(numero<=65535 && (partes[1].equals("X]") || partes[1].equals("Y]") ||
-                                                     partes[1].equals("SP]") || partes[1].equals("PC]"))){
-                                    Evaluar.setADDR("[IDX2]");
-                                }
-                            }
-                        }
-                    break;
+            break;
+            case '%':
                 
-                }
-            }
-            if(Evaluar==FinLinCod){
-                fin=true;
-            }
-            else{
-                Evaluar=Evaluar.getSiguiente();
-            }
-        }while(!fin);
+            break;
+        }
+        return Decimal;
     }
     
-
+    //METODO PARA EVALUAR UN ADDR INMMEDIATO
+    boolean IMM(String operando){
+        boolean imm=false;
+        if(operando.startsWith("#")){
+            imm=true;
+        }
+        return imm;
+    }
+    
+    //METODO PARA EVALUJAR UN ADDR DIR
+        
     public static void main(String[] args) {
         Leer();//Llamo el metodo
-        ADDR(PrimerLinCod);
         new Tabla().setVisible(true);
     }
     
