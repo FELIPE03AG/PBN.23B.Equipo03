8b6025d6b4c369fc7c133480f238a4587c69914c
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2023-10-12 13:54:46 -0500

    [P3](chore)-> Acomodo en MetodosP3 y eliminación de souts de apoyo

--- P3/Parte_3/src/parte_3/MetodosP3.java
+++ P3/Parte_3/src/parte_3/MetodosP3.java
@@ -9,17 +9,16 @@ import java.util.ArrayList;
 public class MetodosP3 {
     static File archivo = new File("P1ASM.lst");
     static File archivo2 = new File("TABSIM.txt");
+    
     static void CrearArchivoList(){ 
         try {
             // Eliminar el archivo si ya existe
             if (archivo.exists()) {
                 archivo.delete();
-                System.out.println("El archivo se va a sobreescribir");
             }
 
             // Crear el archivo nuevamente
             archivo.createNewFile();
-            System.out.println("El archivo se creo correctamente: " + archivo.getName());
 
             RandomAccessFile auxArchivo = new RandomAccessFile("P1ASM.lst", "rw");
             auxArchivo.seek(auxArchivo.length());
@@ -39,18 +38,81 @@ public class MetodosP3 {
         }
     }//fin crear
     
+    public static String sumarHexadecimal(String hexadecimal, int entero) {
+        // Convertir el valor hexadecimal a entero
+        int valorHexadecimal = Integer.parseInt(hexadecimal, 16);
+
+        // Sumar el entero al valor hexadecimal
+        int suma = valorHexadecimal + entero;
+
+        // Obtener la representación hexadecimal del resultado
+        String resultadoHexadecimal = Integer.toHexString(suma).toUpperCase();
+
+        return resultadoHexadecimal;
+    }
+    
+    static void LlenarList(ArrayList <Linea> AuxLineasCodigo) {
+        int bytes=0;
+        String valor="0",valorEqu="0";
+        CrearArchivoList();
+        Creartabsim();
+        try{
+            RandomAccessFile auxArchivo = new RandomAccessFile("P1ASM.lst","rw"); //Encuentro el archivo y accedo para leer y escribir
+            for(Linea auxiliar : AuxLineasCodigo){
+                if(!(auxiliar.getSize().equals(" "))){
+                    auxArchivo.seek(auxArchivo.length()); //Seek posiciona el puntero donde escribir, length es para decirle donde esta el final
+                    if(auxiliar.getCodop().equals("ORG")){
+                        auxArchivo.writeBytes("DIR_INIC,        ");
+                        valor=Parte_3.validarDireccion(auxiliar.getOperando());
+                        auxArchivo.writeBytes(sumarHexadecimal(valor,bytes)+",       ");
+                    }
+                    else if(auxiliar.getCodop().equals("EQU")){
+                        auxArchivo.writeBytes("VALOR,       ");
+                        auxArchivo.writeBytes(Parte_3.validarDireccion(auxiliar.getOperando())+",       ");
+                        auxiliar.setConloc(Parte_3.validarDireccion(auxiliar.getOperando()));
+                    }
+                    else{
+                        if(!(valor.equals("0"))){
+                            auxArchivo.writeBytes("CONTLOC,       ");
+                            valor= sumarHexadecimal(valor,bytes);
+                            auxArchivo.writeBytes(valor+",       ");
+                            auxiliar.setConloc(valor);
+                        }
+                    }
+                    if(auxiliar.getEtiqueta().equals(" ")){
+                        auxArchivo.writeBytes("NULL,       ");
+                    }
+                    else{
+                        auxArchivo.writeBytes(auxiliar.getEtiqueta()+",       ");
+                    }
+                    auxArchivo.writeBytes(auxiliar.getCodop()+",       ");
+                    if(auxiliar.getOperando().equals(" ")){
+                        auxArchivo.writeBytes("NULL,       ");
+                    }
+                    else{
+                        auxArchivo.writeBytes(auxiliar.getOperando()+",       ");
+                    }
+                    auxArchivo.writeBytes("\n");
+                    String[] campos= auxiliar.getSize().split("\\s+");
+                    bytes=Integer.parseInt(campos[0]);
+                }
+            }
+        }catch(IOException ex){
+            System.out.println("ERROR");
+            ex.printStackTrace();
+        }//fin catch
+    }    
+    
     //metodo para crear la tabsim
     static void Creartabsim(){
         try {
             // Eliminar el archivo si ya existe
             if (archivo2.exists()) {
                 archivo2.delete();
-                System.out.println("El archivo TABSIM se va a sobreescribir");
             }
 
             // Crear el archivo nuevamente
             archivo2.createNewFile();
-            System.out.println("El archivo TABSIM se creo correctamente: " + archivo2.getName());
 
             RandomAccessFile auxArchivo = new RandomAccessFile("TABSIM.txt", "rw");
             auxArchivo.seek(auxArchivo.length());
@@ -95,69 +157,4 @@ public class MetodosP3 {
         }//fin catch
     } //fin del metodo llenar tabsim
     
-    public static String sumarHexadecimal(String hexadecimal, int entero) {
-        // Convertir el valor hexadecimal a entero
-        int valorHexadecimal = Integer.parseInt(hexadecimal, 16);
-
-        // Sumar el entero al valor hexadecimal
-        int suma = valorHexadecimal + entero;
-
-        // Obtener la representación hexadecimal del resultado
-        String resultadoHexadecimal = Integer.toHexString(suma).toUpperCase();
-
-        return resultadoHexadecimal;
-    }
-    
-    static void LlenarList(ArrayList <Linea> AuxLineasCodigo) {
-        int bytes=0;
-        String valor="0",valorEqu="0";
-        CrearArchivoList();
-        Creartabsim();
-        try{
-            RandomAccessFile auxArchivo = new RandomAccessFile("P1ASM.lst","rw"); //Encuentro el archivo y accedo para leer y escribir
-            for(Linea auxiliar : AuxLineasCodigo){
-                if(!(auxiliar.getSize().equals(" "))){
-                    auxArchivo.seek(auxArchivo.length()); //Seek posiciona el puntero donde escribir, length es para decirle donde esta el final
-                    if(auxiliar.getCodop().equals("ORG")){
-                        System.out.println("Identifico ORG");
-                        auxArchivo.writeBytes("DIR_INIC,        ");
-                        valor=Parte_3.validarDireccion(auxiliar.getOperando());
-                        auxArchivo.writeBytes(sumarHexadecimal(valor,bytes)+",       ");
-                    }
-                    else if(auxiliar.getCodop().equals("EQU")){
-                        auxArchivo.writeBytes("VALOR,       ");
-                        auxArchivo.writeBytes(Parte_3.validarDireccion(auxiliar.getOperando())+",       ");
-                        auxiliar.setConloc(Parte_3.validarDireccion(auxiliar.getOperando()));
-                    }
-                    else{
-                        if(!(valor.equals("0"))){
-                            auxArchivo.writeBytes("CONTLOC,       ");
-                            valor= sumarHexadecimal(valor,bytes);
-                            auxArchivo.writeBytes(valor+",       ");
-                            auxiliar.setConloc(valor);
-                        }
-                    }
-                    if(auxiliar.getEtiqueta().equals(" ")){
-                        auxArchivo.writeBytes("NULL,       ");
-                    }
-                    else{
-                        auxArchivo.writeBytes(auxiliar.getEtiqueta()+",       ");
-                    }
-                    auxArchivo.writeBytes(auxiliar.getCodop()+",       ");
-                    if(auxiliar.getOperando().equals(" ")){
-                        auxArchivo.writeBytes("NULL,       ");
-                    }
-                    else{
-                        auxArchivo.writeBytes(auxiliar.getOperando()+",       ");
-                    }
-                    auxArchivo.writeBytes("\n");
-                    String[] campos= auxiliar.getSize().split("\\s+");
-                    bytes=Integer.parseInt(campos[0]);
-                }
-            }
-        }catch(IOException ex){
-            System.out.println("ERROR");
-            ex.printStackTrace();
-        }//fin catch
-    }
 }
