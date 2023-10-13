84474c2e0783bd8335a8fdd458d709fd47d1f6cd
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2023-10-12 20:24:58 -0500

    [P3](Chore)-> Uso de asm de p3

--- P3/Parte_3/P1ASM.asm
+++ P3/Parte_3/P1ASM.asm
@@ -1,30 +1,7 @@
-	ORG $1
-Et1: SWI
-dos: EORA 5
-    EORA @5
-Tres: EORA %111
-Pru: EQU @34
-    EORA $6
-    EORA 300
-    EORA $FFF
-    DS.B 4
-    EORA #5
-    EORA #@5
-    EORA #1500
-    EORA 1,X
-Uno: EORA -255,X
-    EORA 32768,X
-    EORA 1,+SP
-    EORA A,X
-    EORA 254,X
-    EORA 64444,X
-    EORA [1,X]
-    EORA [6444,X]
-    EORA [D,X]
-    BLT Uno
-    CPD Dos
-    ADDD #28455
-    ADDD
-    LBLT Uno
-    IBNE A,Uno
-    END
+	ORG	$1010
+	NOP
+	INX	
+Et1:	ADCA	#3
+Tres:	EQU	%111
+Uno:	DS.B	3
+	END
