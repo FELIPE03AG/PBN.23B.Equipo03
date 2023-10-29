7e7979125124e16d0eb929fc1711824f98426993
adrian sanchez <adrian.sanchez6520@alumnos.udg.mx>
2023-10-29 16:12:24 -0600

    [P4.4] (feat) -> Iniciar con la tabla 6

--- P4/Parte_4/src/parte_4/Parte_4.java
+++ P4/Parte_4/src/parte_4/Parte_4.java
@@ -9,10 +9,17 @@
 
 package parte_4;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import static parte_4.Salvacion.IdentificacionDirectivas;
+import static parte_4.Salvacion.IdentificarADDR;
+import static parte_4.Salvacion.Separarform;
+import static parte_4.Salvacion.auxSalvacion;
+import static parte_4.Salvacion.coincidencias;
+import static parte_4.Salvacion.encontrado;
 
 public class Parte_4 {
     static Linea NewLinCod=null; //Para guardar en una lista las lineas del codigo
@@ -225,8 +232,45 @@ public class Parte_4 {
         }
     }
     
+    static String calcularTabla6() {
+    try {
+        RandomAccessFile tablaFile = new RandomAccessFile("Tabla-A_6.txt", "r");
+        String lineaTabla;
+        boolean encontrado = false;
+        long originalPointer = tablaFile.getFilePointer(); 
+
+        while ((lineaTabla = tablaFile.readLine()) != null) {
+            String[] campos = lineaTabla.split("\\s+");
+
+            if (campos.length == 4) {
+                String instruccion = campos[0];
+                String tipo = campos[1];
+                String relativo = campos[2];
+                String lb = campos[3].trim();
+
+                if (NewLinCod.getCodop().equals(instruccion) && NewLinCod.getOperando().equals(tipo) && "Positivo".equals(relativo)) {
+                    encontrado = true;
+                    tablaFile.seek(originalPointer); 
+                    return lb;
+                }
+            }
+            originalPointer = tablaFile.getFilePointer();
+        }
+
+        tablaFile.seek(originalPointer); 
+        if (!encontrado) {
+            return "No se encontró una coincidencia";
+        }
+
+        tablaFile.close();
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+
+    return "No se encontró una coincidencia";
+}
 //******************************************************************** PARTE 2 *************************************************
-    
+        
     //METODO DE APOYO PARA PASAR DE OCTAL A DECIMAL...
     public static int octalADecimal(int octal) {//Este metodo lo usaremos para validar que no pase de los bits permitidos
     int decimal = 0;//vatriable a retornar valor obtenido
