c971866bf5915491af75e6b378bc87bf75ea7e72
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2023-10-30 01:02:30 -0600

    [P4.4](feat)-> Calculo del postbyte de los rel16

--- P4/Parte_4/src/parte_4/CalculoREL.java
+++ P4/Parte_4/src/parte_4/CalculoREL.java
@@ -90,27 +90,97 @@ public class CalculoREL {
                     rr = completarBinarioAByte(rr);//lo completa a byte
                     rr = Salvacion.calcularComplementoDos(rr);//calcula el complemento a 2
                     rr = Integer.toHexString(Parte_4.ConvertirADecimal("%".concat(rr))).toUpperCase();//guarda el resultado en hexadecimal
-                    postbyte = postbyte.concat(" ").concat(rr);//Concatena lo que calculamos con lo que se tenia
                 }//fin rango valido
-            } else if (dest > ori) {//o si el destino es mayor al origen
+            } 
+            else if (dest > ori) {//o si el destino es mayor al origen
                 resta = dest - ori;//Hace la resta correspondiente 
                 if (resta < 128) {//valida el rango del salto positivo
                     rr = Integer.toHexString(resta).toUpperCase();//guarda el resultado en hexadecimal
-                    if (rr.length() == 1) {//si no es menos de un byte
-                        rr = "0".concat(rr);//completa a expresion en byte
-                    }//fin no es un byte
-                    postbyte = postbyte.concat(" ").concat(rr);//concatena lo calculado con lo que se tenia
                 }//fin rango correcto
             }//fin destino mayor al origen
+            else if(dest == ori){
+                rr="00";
+            }
+            if (rr.length() == 1) {//si no es menos de un byte
+                rr = "0".concat(rr);//completa a expresion en byte
+            }//fin no es un byte
+            postbyte = postbyte.concat(" ").concat(rr);//Concatena lo que calculamos con lo que se tenia
         }//fin direccion valida
         relativo.setCop(postbyte);//guardo el postbyte completo en los datos de la instruccion
-    }//Fin postbyte de lo rel 8 y 16
+    }//Fin postbyte de lo rel 8
+    
+    public static void postREL16(Linea relativo, String origen){
+        String postbyte = " ";//Variable que guarda el postbyte de la instruccion
+        String rr = " ";//Variable que guarda la parte que calculamos
+        String destino=" ";//Direccion a la que va el brinco
+        int ori, dest,resta;//Variables auxiliares para la resta de direcciones
+        boolean destinoExist=false; //Variable que verifica que el destino existe
+       
+        destino=conlocEtq(relativo.getOperando());//Se guarda la direccion de destino
+        if (absoluta){//Si es absoluta revisa que si exista una instruccion en esa posicion
+            for(Linea existe:Parte_4.LineasASM){
+                if(existe.getConloc().equals(destino)){
+                    destinoExist=true;//enciende la bandera
+                }//fin si existe esa direccion con una instruccion
+            }//fin for, buscar la direccion
+            if(!destinoExist){
+                destino=" ";//limpia el destino
+            }//Fin no existe
+            absoluta=false;//reinicia la bandera
+        }//fin es absoluta
+        
+        String frmbase [] = relativo.getForm().split(",");//separa el frmbase en calculado y por calcular
+        if (!(destino.equals(" "))) {//si existe una direccion de memoria
+            postbyte = frmbase[0].concat(" ").concat(frmbase[1]);//guarda la parte que ya esta calculada
+            ori = Parte_4.ConvertirADecimal("$".concat(origen));//guarda el valor decimal del origen
+            dest = Parte_4.ConvertirADecimal("$".concat(destino));//guarda el valor decimal del destino
+            if (ori > dest) {//Si origen es mayor que el destino
+                resta = ori - dest;//restar el destino al origen
+                if (resta < 32769) {//validacion del rango del salto negativo
+                    rr = decimalABinario(resta);//Paso de decimal a binario el resultado de la resta
+                    rr = completarBinarioAByte(rr);//lo completa a byte
+                    rr = Salvacion.calcularComplementoDos(rr);//calcula el complemento a 2
+                    rr = Integer.toHexString(Parte_4.ConvertirADecimal("%".concat(rr))).toUpperCase();//guarda el resultado en hexadecimal
+                }//fin rango valido
+            } 
+            else if (dest > ori) {//o si el destino es mayor al origen
+                resta = dest - ori;//Hace la resta correspondiente 
+                if (resta < 65535) {//valida el rango del salto positivo
+                    rr = Integer.toHexString(resta).toUpperCase();//guarda el resultado en hexadecimal
+                }//fin rango correcto
+            }//fin destino mayor al origen
+            else if (dest == ori) {
+                rr = "0000";
+            }
+            switch (rr.length()) {
+            //fin no es un byte
+            case 1:
+            //si no es menos de un byte
+                rr = "000".concat(rr);//completa a expresion en byte
+            break;
+            case 2:
+                rr = "00".concat(rr);//completa a expresion en byte
+            break;
+            case 3:
+                rr = "0".concat(rr);//completa a expresion en byte
+            break;
+            default:
+            break;
+            }//fin switch
+        }//fin direccion valida
+        rr = rr.substring(0, 2).concat(" ").concat(rr.substring(2, 4));//divide por byte
+        postbyte = postbyte.concat(" ").concat(rr);//concatena lo calculado con lo que se tenia
+        relativo.setCop(postbyte);//guardo el postbyte completo en los datos de la instruccion
+    }//Fin postbyte de lo rel 16
     
     static void buscarRels(){//Metodo para identificar rels y calcular su postbyte
         for(Linea asm:Parte_4.LineasASM){//for para recorrer todas las intrucciones
             if(asm.getADDR().equals("REL (8b)")){//si es relativa de 8b
                 postREL(asm, Conloc.sumarHexadecimal(asm.getConloc(), 2));//calcula su postbyte
             }//fin relativo de 8b
+            else if(asm.getADDR().equals("REL (16b)")){//Si es relativo de 16b
+                postREL16(asm, Conloc.sumarHexadecimal(asm.getConloc(), 4));//calcula su postbyte
+            }//fin rel16b
         }//fin for asm
     }//Fin buscar rels
-}//Fin de la calse
+}//Fin de la clase
