4e370d058d0285e5d4bb07467879d9114cfd568d
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2023-10-30 00:25:09 -0600

    [P4.4](fix)-> Postbyte de los rel8 y (chore) comentarios en CalculoREL

--- P4/Parte_4/src/parte_4/CalculoREL.java
+++ P4/Parte_4/src/parte_4/CalculoREL.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package parte_4;
 
 public class CalculoREL {
@@ -9,25 +5,25 @@ public class CalculoREL {
     
     //Busqueda de direccion de la etiquta
     static String conlocEtq(String etiqueta){
-        String direccion = " ";
-        for(Tabsim etq:Conloc.etiquetas){
-            if(etq.getSi().equals(etiqueta)){
-                direccion=etq.getTi();
-                if(etq.tipo.equals("ABSOLUTA")){
-                    absoluta=true;
-                }
-            }
-        }
+        String direccion = " ";//variable que guarda la direccion
+        for(Tabsim etq:Conloc.etiquetas){//busca en el arreglo con los datos del tabsim
+            if(etq.getSi().equals(etiqueta)){//compara con las etiquetas
+                direccion=etq.getTi();//Guarda la direccion que corresponde a esa etiqueta
+                if(etq.tipo.equals("ABSOLUTA")){//Si es una etiqueta que pertenece a equ
+                    absoluta=true;//activa la bandera
+                }//Es una etiqueta absoluta
+            }//Se encontro la etiqueta
+        }//For para recorrer todas las etiquetas
         return direccion;
-    }
+    }//Fin del metodo para encontrar el conloc de una etiqueta
+    
     //metodo resta hexadecimal
     static String restaHex(String conloc, String conloc2){
-       int valor1 = Integer.parseInt(conloc, 16);
-       int valor2 = Integer.parseInt(conloc2, 16);
-       int res = valor1 - valor2;
-       String resta = Integer.toHexString(res).toUpperCase();
+       int valor1 = Integer.parseInt(conloc, 16);//Pasa a entero el valor hexadecimal
+       int valor2 = Integer.parseInt(conloc2, 16);//Valor dos guarda el sustraendo
+       int res = valor1 - valor2;//Se hace la resta y se guarda en res
+       String resta = Integer.toHexString(res).toUpperCase();//Se pasa el resultado a hexadecimal
        return resta;
-        
     }//fin metodo resta hexadecimal
     
     // Función para completar un número binario a 8 bits (un byte)
@@ -45,68 +41,76 @@ public class CalculoREL {
         } else {
             return binario.substring(longitud - 8); // Toma los últimos 8 bits del número binario si tiene más de 8 bits
         }
-    }
+    }//Fin metodo completar a binario en byte
+    
+    public static String decimalABinario(int numeroDecimal) {
+        if (numeroDecimal == 0) {
+            return "0"; // Si el número es 0, su representación binaria también es 0
+        }
+
+        StringBuilder binario = new StringBuilder();
+        while (numeroDecimal > 0) {
+            int residuo = numeroDecimal % 2; // Obtiene el residuo de dividir por 2
+            binario.insert(0, residuo); // Inserta el residuo al inicio del StringBuilder
+            numeroDecimal = numeroDecimal / 2; // Divide el número por 2 para la próxima iteración
+        }
+
+        return binario.toString();
+    }//fin decimal a binario
     
     public static void postREL(Linea relativo, String origen){
-        String postbyte = " ";
-        String rr = " ";
-        String destino=" ";
-        int ori, dest,resta;
-        boolean destinoExist=false;
-        
-        destino=conlocEtq(relativo.getOperando());
-        if (absoluta){
+        String postbyte = " ";//Variable que guarda el postbyte de la instruccion
+        String rr = " ";//Variable que guarda la parte que calculamos
+        String destino=" ";//Direccion a la que va el brinco
+        int ori, dest,resta;//Variables auxiliares para la resta de direcciones
+        boolean destinoExist=false; //Variable que verifica que el destino existe
+       
+        destino=conlocEtq(relativo.getOperando());//Se guarda la direccion de destino
+        if (absoluta){//Si es absoluta revisa que si exista una instruccion en esa posicion
             for(Linea existe:Parte_4.LineasASM){
                 if(existe.getConloc().equals(destino)){
-                    destinoExist=true;
-                }
-            }
+                    destinoExist=true;//enciende la bandera
+                }//fin si existe esa direccion con una instruccion
+            }//fin for, buscar la direccion
             if(!destinoExist){
-                destino=" ";
-            }
-            absoluta=false;
-        }
+                destino=" ";//limpia el destino
+            }//Fin no existe
+            absoluta=false;//reinicia la bandera
+        }//fin es absoluta
         
-        String frmbase [] = relativo.getForm().split(",");
-        if (!(destino.equals(" "))) {
-            postbyte = frmbase[0];
-            ori = Parte_4.ConvertirADecimal("$".concat(origen));
-            dest = Parte_4.ConvertirADecimal("$".concat(destino));
-            if (ori > dest) {
-                System.out.println("SI ENTRO");
-                resta = ori - dest;
-                System.out.println("resta en decimal = "+resta);
-                if (resta < 129) {
-                    rr = Salvacion.decimalABinario(resta);
-                    System.out.println("en binario = "+rr);
-                    rr = completarBinarioAByte(rr);
-                    System.out.println("en byte = "+rr);
-                    rr = Salvacion.calcularComplementoDos(rr);
-                    System.out.println("Complemento = "+rr);
-                    rr = Integer.toHexString(Parte_4.ConvertirADecimal(rr)).toUpperCase();
-                    postbyte = postbyte.concat(" ").concat(rr);
-                }
-            } else if (dest > ori) {
-                resta = dest - ori;
-                if (resta < 128) {
-                    rr = Salvacion.decimalABinario(resta);
-                    rr = Integer.toHexString(Parte_4.ConvertirADecimal(rr)).toUpperCase();
-                    if (rr.length() == 1) {
-                        rr = "0".concat(rr);
-                    }
-                    postbyte = postbyte.concat(" ").concat(rr);
-                }
-            }
-
-        }
-        relativo.setCop(postbyte);
+        String frmbase [] = relativo.getForm().split(",");//separa el frmbase en calculado y por calcular
+        if (!(destino.equals(" "))) {//si existe una direccion de memoria
+            postbyte = frmbase[0];//guarda la parte que ya esta calculada
+            ori = Parte_4.ConvertirADecimal("$".concat(origen));//guarda el valor decimal del origen
+            dest = Parte_4.ConvertirADecimal("$".concat(destino));//guarda el valor decimal del destino
+            if (ori > dest) {//Si origen es mayor que el destino
+                resta = ori - dest;//restar el destino al origen
+                if (resta < 129) {//validacion del rango del salto negativo
+                    rr = decimalABinario(resta);//Paso de decimal a binario el resultado de la resta
+                    rr = completarBinarioAByte(rr);//lo completa a byte
+                    rr = Salvacion.calcularComplementoDos(rr);//calcula el complemento a 2
+                    rr = Integer.toHexString(Parte_4.ConvertirADecimal("%".concat(rr))).toUpperCase();//guarda el resultado en hexadecimal
+                    postbyte = postbyte.concat(" ").concat(rr);//Concatena lo que calculamos con lo que se tenia
+                }//fin rango valido
+            } else if (dest > ori) {//o si el destino es mayor al origen
+                resta = dest - ori;//Hace la resta correspondiente 
+                if (resta < 128) {//valida el rango del salto positivo
+                    rr = Integer.toHexString(resta).toUpperCase();//guarda el resultado en hexadecimal
+                    if (rr.length() == 1) {//si no es menos de un byte
+                        rr = "0".concat(rr);//completa a expresion en byte
+                    }//fin no es un byte
+                    postbyte = postbyte.concat(" ").concat(rr);//concatena lo calculado con lo que se tenia
+                }//fin rango correcto
+            }//fin destino mayor al origen
+        }//fin direccion valida
+        relativo.setCop(postbyte);//guardo el postbyte completo en los datos de la instruccion
     }//Fin postbyte de lo rel 8 y 16
     
-    static void buscarRels(){
-        for(Linea asm:Parte_4.LineasASM){
-            if(asm.getADDR().equals("REL (8b)")){
-                postREL(asm, Conloc.sumarHexadecimal(asm.getConloc(), 2));
-            }
-        }
-    }
-}
+    static void buscarRels(){//Metodo para identificar rels y calcular su postbyte
+        for(Linea asm:Parte_4.LineasASM){//for para recorrer todas las intrucciones
+            if(asm.getADDR().equals("REL (8b)")){//si es relativa de 8b
+                postREL(asm, Conloc.sumarHexadecimal(asm.getConloc(), 2));//calcula su postbyte
+            }//fin relativo de 8b
+        }//fin for asm
+    }//Fin buscar rels
+}//Fin de la calse
