e3b7311dd1163e58083105a687eda3d9f3f4817f
Samuel_xD <samuel.angel7650@alumnos.udg.mx>
2023-10-29 23:10:19 -0600

    [P4.4](feat) --> Boton en clase tabla, abre el buscador de archivos

--- P4/Parte_4/src/parte_4/Tabla.java
+++ P4/Parte_4/src/parte_4/Tabla.java
@@ -1,13 +1,19 @@
 package parte_4;
 
 
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 
 public class Tabla extends javax.swing.JFrame {
     DefaultTableModel diseño=new DefaultTableModel();
+    static JFileChooser explorador = new JFileChooser();
+    static String dirtmp = "";
     
     //METODO PARA LLENAR LA TABLA
     void Llenado(){
@@ -16,6 +22,22 @@ public class Tabla extends javax.swing.JFrame {
                                         auxiliar.getSize(),auxiliar.getPorCalcular(), auxiliar.getForm(), auxiliar.getCop()});
         }
     }
+    
+    static void nuevoAsm(){
+        try{
+            explorador.addChoosableFileFilter(new FileNameExtensionFilter("Ensamblador", "asm"));
+            explorador.showOpenDialog(null);//ventana de diálogo esta inicializada en null
+            File auxFile;
+            auxFile = explorador.getSelectedFile();
+            dirtmp = auxFile.getAbsolutePath();
+            
+            
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null,"Error al abrir el archivo","Advertencia",JOptionPane.WARNING_MESSAGE);
+        }
+
+    }
+    
     public Tabla() {
         initComponents();
         String[] titulo = new String[]{"CONLOC","ETQ","CODOP","OPR","ADDR","SIZE","POR CALCULAR","FORM","COP"};
@@ -35,9 +57,14 @@ public class Tabla extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         TablaCod = new javax.swing.JTable();
+        btnArchivo = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,17 +92,34 @@ public class Tabla extends javax.swing.JFrame {
         TablaCod.setSelectionBackground(new java.awt.Color(0, 204, 204));
         jScrollPane1.setViewportView(TablaCod);
 
+        btnArchivo.setText("Abrir");
+        btnArchivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnArchivoActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Abrir archivo distinto -->");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 815, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnArchivo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 503, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnArchivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 475, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -92,8 +136,17 @@ public class Tabla extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnArchivoActionPerformed
+        nuevoAsm();
+        initComponents();
+        //Llenado();
+    }//GEN-LAST:event_btnArchivoActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable TablaCod;
+    private javax.swing.JButton btnArchivo;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
