1227beef55256be8e342474172513be3f1048364
Samuel_xD <samuel.angel7650@alumnos.udg.mx>
2023-11-18 20:31:26 -0600

    [P5](feat) --> Agregar S0

--- P5/Parte_4/src/parte_5/Proceso_S19.java
+++ P5/Parte_4/src/parte_5/Proceso_S19.java
@@ -1,6 +1,7 @@
 package parte_5;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,19 +13,22 @@ import java.util.ArrayList;
  * @author anaya
  */
 public class Proceso_S19 {
-    static ArrayList <S19> Datos = new ArrayList<>();
+    static List <S19> Datos = new ArrayList<>();
     static S19 AuxS19;
-  
+        
+        
+    
         public static void NombreASM(String nombreArchivo) {
-        System.out.print("data: ");
-        for (int i = 0; i < nombreArchivo.length(); i++) {
-            char caracter = nombreArchivo.charAt(i);
-            int codigoASCII = (int) caracter;
-            String hexa = Integer.toHexString(codigoASCII).toUpperCase();  // Convertir a mayúsculas
-            System.out.print(hexa + " ");
+            System.out.print("Data: ");
+            
+            for (int i = 0; i < nombreArchivo.length(); i++) {
+                char caracter = nombreArchivo.charAt(i);
+                int codigoASCII = (int) caracter;
+                String hexa = Integer.toHexString(codigoASCII).toUpperCase();  // Convertir a mayúsculas
+                System.out.print(hexa + " ");           
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
 
     public static void Data(String data) {
         NombreASM(data);
@@ -36,5 +40,14 @@ public class Proceso_S19 {
         // Asegurarse de que el resultado tenga siempre dos dígitos
         String sumaDeBytes = String.format("%02X", resultado);
         return sumaDeBytes;
+    }
+    
+    public static String ck(String x){
+        
+        return x;
+    }
+    
+    public static void S0 (String nombreArch, String ccS0, String dtaS0, String ckS0){
+        Datos.add(new S19(nombreArch, "S0", ccS0, "00 00", dtaS0, ckS0));
     }
 }
